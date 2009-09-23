1300b29c4fffd3abcd426f0da99934a5eb6013b7
Joshaven Potter <joshaven@joshaven.(none)>
2009-09-23 18:51:16 -0400

    add :<< :halt & :stop, write :when_modified, Update recursive find method

--- lib/FileMonitor.rb
+++ lib/FileMonitor.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/FileMonitor/store'
+require 'find' # needed for the :files_recursive method
 
 # Purpose:
 # Watches the file system for changes
@@ -25,7 +26,6 @@ require File.dirname(__FILE__) + '/FileMonitor/store'
 #   FileMonitor.when_modified(Dir.pwd, "/path/to/other/file.rb") do |watched_item| ... end
 class FileMonitor
   VERSION = '0.0.2'
-  attr_accessor :spawns
   # The new method may be called with an optional callback which must be a block 
   # either do...end or {...}.
   # The block may consiste of upto arguments ie {|watched_item, monitored|}, in which case, the
@@ -52,107 +52,202 @@ class FileMonitor
     @watched = []
   end
   
-  # FileMonitor.when_modified(Dir.pwd, "/path/to/other/file.rb") do |watched_item| ... end
-  def when_modified(*paths, &callback)
-    puts "paths: #{paths.inspect}\ncallback.nil?: #{callback.nil?}"
-    # paths.each do |path|
-    #   
-    # end
+  # Returns the process id of a FileMonitor which is watching the given paths.  The process automatically
+  # calls the given callback when changes are found.
+  #
+  # Example:
+  #   fm = FileMonitor.when_modified(Dir.pwd, "/path/to/other/file.rb") do |watched_item| ... end   #=> 12980
+  def self.when_modified(*paths, &callback)
+    fm = FileMonitor.new &callback
+    paths.each {|path| fm << path}
+    return fm
   end
   
+  # Stops a spawned FileMonitor instance.  The FileMonitor will finish the the currnet iteration and exit gracefully.
+  def stop()
+    # command, pid, ppid = `ps -p #{process_id} -o "comm pid ppid"|grep #{process_id}`.split
+    # Process.kill('INT', process_id) if Process.pid == ppid # kill only if My process is the Parent process
+    
+    # Send user defined signal USR1 to process.  This is trapped in spauned processes and tells the process to Ctrl+C
+    # The user defined signial is sent as a safty percausion because the process id is not tracked through a pid file
+    # nor compared with the running command.  The FileMonitor spaun will respond to the USR1 signal by exiting properly.*
+    if Fixnum === @spawns
+      Process.kill('USR1', @spawns)
+      Process.wait @spawns
+    end
+  end
+  
+  # Halts a spawned FileMonitor Instance.  The current iteration will be halted in its tracks.
+  def halt()
+    if Fixnum === @spawns
+      Process.kill('USR2', @spawns)
+      Process.wait @spawns
+    end
+  end
+
+  # The add method accepts a directory path or file path and optional callback.  If a directory path is given all files in that 
+  # path are recursively added.  If a callback is given then that proc will be called when a change is detected on that file or 
+  # group of files.  If no proc is given via the add method then the object callback is called.
+  #
+  # Example:
+  #   fm = FileMonitor.new do |path|
+  #     puts "Detected a change on #{path}"
+  #   end
+  #
+  #   # The following will run the default callback when changes are found in the /tmp folder:
+  #   fm.add '/tmp'
+  #
+  #   # The following will run its own callback on changed files in the /home folder:
+  #   fm.add '/home' do |path|
+  #     puts "A users file has changed: #{path}"
+  #   end
   def add(path, &callback)
     if File.file? path
       i = MonitoredItems::Store.new
-      i.file      File.expand_path(path)
+      i.path      File.expand_path(path)
       i.callback  callback
-      @watched << i
+      index = index_of(path) || @watched.size
+      @watched[index] = i
       # @watched << MonitoredItems::Store.new({:file => File.expand_path(path), :callback => callback})
       return true
     elsif File.directory? path
-      files_recursive(path).each {|f| add(f, &callback) }
+      files_recursive(path).each {|f| self << f &callback }
       return true
     end
     false
   end
-
-  def changes?
-    return first_changed
+  
+  # The '<<' method works the same way as the 'add' method but does not support a callback.
+  #
+  # Example:
+  #   fm = FileMonitor.new do |path|
+  #     puts "Detected a change on #{path}"
+  #   end
+  #
+  #   # The following will run the default callback when changes are found in the /tmp folder:
+  #   fm << '/tmp'
+  def <<(path)
+    add path
   end
+  
+  # def changes?
+  #   return first_changed
+  # end
 
+  # Itterates watched files and runs callbacks when changes are detected.
   def process
-    # itterates watched files and runs callbacks when changes are detected.
     @watched.each do |i|
-      key = digest(i.file)
+      key = digest(i.path)
       i.digest key if i.digest.nil?  # skip first change detection, its always unknown on first run
-      update(i, key) unless i.digest == key
+      respond_to_change(i, key) unless i.digest == key
     end
   end
 
+  # Returns an Array of files being watched.  Each instance is a MonitoredItems::Store Object
   def watching
-    # Returns an Array of files being watched
     @watched
   end
 
   def monitor(interval = 1)
     trap("INT") do 
-      STDERR.puts "  Interrupted by Control-C"
-      exit 2
+      STDERR.puts "  FileMonitor was interrupted by Control-C"
+      exit
+    end
+    
+    trap("USR1") do
+      STDERR.puts "  FileMonitor was asked nicely to stop."
+      @stop = true
+      @spawns = nil
     end
 
+    trap("USR2") do
+      STDERR.puts "  FileMonitor was halted."
+      @spawns = nil
+      exit
+    end
+
+    
     while true
+      exit if @stop
       process
-      sleep interval
+      sleep interval unless @stop
     end
   end
   
-  # Need to finish... needs to return true/false regarding the existance of the path being watched
-  def watching?(path)
-    
+  # Returns index of watching item or false if non existant.
+  def index_of(path)
+    watching.each_with_index {|watched,i| return i if watched.path == path}
+    false
   end
 
+  # Spauns a child process that is looking for changes at every given interval.  
+  # The interval is in seconds and defaults to 1 second.
   def spawn(interval = 1)
     if @spawns.nil? 
       @spawns = fork {monitor interval}
       Process.detach(@spawns)
+      
       Kernel.at_exit do
-        Process.kill('HUP', @spawns)
+        Process.kill('HUP', @spawns) unless `ps -p #{@spawns}|grep ^[0-9]`.split().empty?
         @spawns = nil
       end
-      true
+      @spawns
     else
       @spawns
     end
   end
+  
+  
 private
-  def update(item, key)
-    item.callback.call(item) unless item.callback.nil?
-    item.digest key
+  attr_accessor :spawns, :callback
+  # Call callback and update digest with given key.
+  def respond_to_change(item, key)
+    if Proc === item.callback     # Use watched instance callback if possible.
+      call item.callback, item, self, key
+    elsif Proc === self.callback  # Use object level callback if possible.
+      call self.callback, item, self, key
+    end
+    item.digest = key
   end
-  # def update(item, key)
-  #   item.callback.call( *(item.callback.arity==2 ? [item, self] : [item]) )
-  #   item.digest key
+  
+  # Calls a proc with no more then the needed arguments.  This avoids an error when
+  # calling a proc with two arguments when the proc only handels one argument.
+  def call(proc, *args)
+    proc.call(args[0..(proc.arity-1)])
+  end
+
+  # Itterates through watched items and returns the first changed or false if no changes are found.
+  # def first_changed
+  #   # returns first changed item or false
+  #   @watched.each {|i| return i if changed? i}
+  #   false
   # end
-
-  def first_changed
-    # returns first changed item or false
-    @watched.each {|i| return i if changed? i}
-    false
-  end
-
+ 
+  # Returns new digest if changed or false if unchanged.
   def changed?(item)
-    # returns md5 if changed, returns false when not changed
-    md5 = digest(item.file) 
-    md5 == item.digest ? false : md5
+    d = digest(item.path)         # get current digets
+    d == item.digest ? false : d  # return new digest or false if unchanged
   end
 
+  # Returns a string representation of the file state.
   def digest(file)
-    # returns the md5 of a file
-    # Digest::MD5.hexdigest( File.read(file) )
     File.mtime(file)
   end
 
-  def files_recursive(dirname)
-    # return an array of files from this (dirname) point forth.
-    Dir["#{dirname}/**/**"].collect {|f| f if File.file? f }.compact
+  # Returns an array of all files in dirname recursively.  Accepts an optional file name regexp filter.
+  # The following will find files ending in ".rb" recursively beginning in the working dir:
+  #   files_recursive Dir.pwd, /\.rb$/
+  def files_recursive(dirname, file_name_regexp=/.*/)
+    paths = []
+    
+    Find.find(dirname) do |path|
+      if FileTest.directory?(path)
+        Find.prune if File.basename(path)[0] == ?. # Don't look any further into directies beginning with a dot.
+      else
+        paths << path if file_name_regexp === path # Amend the return array if the file found matches the regexp
+      end
+    end
+    
+    return paths
   end
 end
